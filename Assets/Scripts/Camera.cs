ec77f2bf7f3e8ae1c91be6b8e82b6391c0eeaa61
Seiji Emery <seijiemery@gmail.com>
2019-10-23 12:48:57 -0700

    cleanup

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera : MonoBehaviour {
     public Transform target;
-    public Movement player;
     
     // Start is called before the first frame update
     void Start()
@@ -15,15 +15,12 @@ public class Camera : MonoBehaviour {
     public float factor = 0.5f;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (target != null) {
             transform.LookAt(target);
-            
-
-            var targetPos = transform.position + transform.rotation * Vector3.back * 5.0f
-                                               + Vector3.up * 2f;
-transform.position = Vector3.Lerp(transform.position, targetPos, 
+            var offset = Vector3.up * 2f + transform.rotation * Vector3.back * 5f;
+            var targetPos = transform.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 
                 Mathf.Clamp(Time.deltaTime * this.factor, 0f, 1f));
 
         }
